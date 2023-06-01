f7ac31ee450d1d096c33dbd49f86a50069403aba
Aleksandr Zimin <uzao-rec@ya.ru>
2023-06-02 02:48:25 +0300

    feature: another classes and webpack settingd

---
+++ src/scripts/Section.js
@@ -0,0 +1,14 @@
+export default class Section {
+  constructor(renderer, selector) {
+    this._renderer = renderer;
+    this._container = document.querySelector(selector);
+  }
+
+  rendererElement(card) {
+    this._container.prepend(card);
+  }
+
+  rendererElements(cards) {
+    cards.forEach(this._renderer);
+  }
+}
